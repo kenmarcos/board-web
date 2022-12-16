006503cc0c04e30a7c2359c8841fba4f1d7a1eff
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2022-12-16 17:46:07 -0300

    add next auth settings

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -1,24 +1,34 @@
 import styles from "components/LoginButton/styles.module.scss";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export const LoginButton = () => {
-  const isLoggedIn = false;
+  const { data: session } = useSession();
+
+  const handleSession = () => {
+    if (session) {
+      signOut();
+    } else {
+      signIn("github");
+    }
+  };
 
   return (
-    <button type="button" className={styles.loginButton} onClick={console.log}>
-      {isLoggedIn ? (
+    <button
+      type="button"
+      className={styles.loginButton}
+      onClick={handleSession}
+    >
+      {session ? (
         <picture>
-          <img
-            src="http://sujeitoprogramador.com/steve.png"
-            alt="Foto do usuário"
-          />
+          <img src={session.user?.image as string} alt="Foto do usuário" />
         </picture>
       ) : (
         <FaGithub color="#FFB800" />
       )}
-      Entrar com GitHub
-      {!!isLoggedIn && <FiX color="#737380" />}
+      {!!session ? `Olá ${session.user?.name}` : "Entrar com GitHub"}
+      {!!session && <FiX color="#737380" />}
     </button>
   );
 };
