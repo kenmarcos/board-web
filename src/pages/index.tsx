4f9f640090a139d5f64012f05c0a4203088509f0
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2022-12-16 13:39:56 -0300

    create home page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
 import styles from "styles/styles.module.scss";
 
@@ -7,9 +8,46 @@ export default function Home() {
       <Head>
         <title>Board - Organizando seu dia</title>
       </Head>
-      <h1 className={styles.title}>
-        Trabalhando com <span>NextJS</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <picture>
+          <img src="/images/board-user.svg" alt="Ferramenta Board" />
+        </picture>
+
+        <section className={styles.callToAction}>
+          <h1>
+            Uma ferramenta para seu dia a dia Escreva, planeje e organize-se...
+          </h1>
+          <p>
+            <span>100% Gratuita</span> e Online
+          </p>
+        </section>
+
+        <div className={styles.donaters}>
+          <h3>Apoiadores:</h3>
+          <div>
+            <img
+              src="http://sujeitoprogramador.com/steve.png"
+              alt="Usuário 1"
+            />
+            <img
+              src="http://sujeitoprogramador.com/steve.png"
+              alt="Usuário 1"
+            />
+            <img
+              src="http://sujeitoprogramador.com/steve.png"
+              alt="Usuário 1"
+            />
+            <img
+              src="http://sujeitoprogramador.com/steve.png"
+              alt="Usuário 1"
+            />
+            <img
+              src="http://sujeitoprogramador.com/steve.png"
+              alt="Usuário 1"
+            />
+          </div>
+        </div>
+      </main>
     </>
   );
 }
