7e6d1902e9de6e42c7b105a09fb379ca90bff69e
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2022-12-16 11:08:40 -0300

    add global styles settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,9 @@ import styles from "styles/styles.module.scss";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Board - Organizando seu dia</title>
+      </Head>
       <h1 className={styles.title}>
         Trabalhando com <span>NextJS</span>
       </h1>
